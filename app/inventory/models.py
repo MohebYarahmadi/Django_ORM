129e2c09eebcc09c9acd4f7fe02983b4952f79c5
MohebYarahmadi <moheb.yarahmadi@gmail.com>
2026-07-23 18:40:46 +0330

    Basic Model Migrations done

--- app/inventory/models.py
+++ app/inventory/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+# -------------------------------------
+# Category Model
+# -------------------------------------
+class Category(models.Model):
+	pass
+
+	class Meta:
+		db_table = 'category'
+
+
+# -------------------------------------
+# Promotion Event Model
+# -------------------------------------
+class PromotionEvent(models.Model):
+	pass
+
+
+# -------------------------------------
+# Product Model
+# -------------------------------------
+class Product(models.Model):
+	pass
+
+
+# -------------------------------------
+# Product Promotion Event Model
+# -------------------------------------
+class ProductPromotionEvent(models.Model):
+	pass
+
+
+# -------------------------------------
+# Stock Management Model
+# -------------------------------------
+class StockManagement(models.Model):
+	pass
+
+
+# -------------------------------------
+# User Model
+# -------------------------------------
+class User(models.Model):
+	pass
+
+
+# -------------------------------------
+# Order Model
+# -------------------------------------
+class Order(models.Model):
+	pass
+
+
+# -------------------------------------
+# Order Product Model
+# -------------------------------------
+class OrderProduct(models.Model):
+	pass
