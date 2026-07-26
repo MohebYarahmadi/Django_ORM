b77e0f5437ff1c767eedafd5872f750b3febfa86
MohebYarahmadi <moheb.yarahmadi@gmail.com>
2026-07-26 13:33:59 +0330

    Register Admin Basic

--- app/inventory/admin.py
+++ app/inventory/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import (
+	Category, Product, Order, OrderProduct,
+	ProductPromotionEvent, PromotionEvent,
+	StockManagement, User
+)
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
+admin.site.register(ProductPromotionEvent)
+admin.site.register(PromotionEvent)
+admin.site.register(StockManagement)
+admin.site.register(User)
