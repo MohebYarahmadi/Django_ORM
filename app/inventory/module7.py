cd3c49a1c3185cf8ad430d3b1a5729324fc6e03c
MohebYarahmadi <moheb.yarahmadi@gmail.com>
2026-08-18 11:14:48 +0330

    Inner Join with 1:M relationships

--- app/inventory/module7.py
+++ app/inventory/module7.py
@@ -14,5 +14,32 @@ from inventory.models import (
 router = Router()
 
 #region START
+# =====================================
+# Product + Category: Return product and category info by product IDs
+# =====================================
+class CategoryNested(Schema):
+	id: int
+	name: str
+	slug: str
+	level: int
+	is_active: bool
+
+class ProductWithCategoryOut(Schema):
+	id: int
+	name: str
+	slug: str
+	price: float
+	is_active: bool
+	category: CategoryNested
+
+@router.get(
+	'/products/by-id/',
+	tags=['modlue7'],
+	summary='Return product and category info by product ids',
+	response=list[ProductWithCategoryOut]
+)
+def get_products_by_id(request, ids: List[int] = Query(...)):
+	qs = Product.objects.select_related('category').filter(id__in=ids)
+	return qs
 
 #endregion START
